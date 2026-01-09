7f69ddf2595623723f4e0603ea3d4c99601dba07
Lucas David <lucas_david04@outlook.com>
2026-01-09 12:37:21 -0300

    alter filter of home view

--- atlas_app/views.py
+++ atlas_app/views.py
@@ -1,8 +1,8 @@
 from django.http import JsonResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import generic
 import requests
-
+from django.db.models import Q
 from .models import Lançamentos, Popular, Genres, Media
 
 
@@ -24,25 +24,42 @@ def chunk_list(lst, n):
 
 # Create your views here.
 def homeView(request):
-    
-    if request.method == 'GET':
-        last_add = Lançamentos.objects.all().order_by('post_date')
-        populars = Popular.objects.all()
-        last = Media.objects.all().order_by('-lancament_date')[:10]
-        if not last_add:
-            pages = list(chunk_list(last,6))
+    last_add = Lançamentos.objects.all().order_by('post_date')
+    populars = Popular.objects.all()
+    last = Media.objects.all().order_by('-lancament_date')[:10]
+
+    # Paginação em chunks de 6
+    if not last_add:
+        pages = list(chunk_list(last, 6))
+    else:
+        pages = list(chunk_list(last_add, 6))
+
+    # Filtro de busca
+    query = request.GET.get('gender')  # seu campo de input
+    if query:
+        # Tenta encontrar no modelo de filmes ou séries
+        movie_match = Media.objects.filter(
+            Q(title__icontains=query), typeMedia='filme'
+        ).first()
+        series_match = Media.objects.filter(
+            Q(title__icontains=query), typeMedia='serie'
+        ).first()
+
+        if movie_match:
+            # Redireciona para a view de filmes, passando query
+            return redirect('movies_media')  # Substitua 'movies_list' pelo nome da sua URL
+        elif series_match:
+            # Redireciona para a view de séries
+            return redirect('series_media')  # Substitua 'series_list' pelo nome da sua URL
         else:
-            pages = list(chunk_list(last_add,6))
+            # Nenhum resultado encontrado, permanece na home ou mostra mensagem
+            pages = []  # limpa resultados
 
-        filter = request.GET.get('gender')
-
-
-        fit = {'filltler':filter}
-
-        
-
-
-    return render(request, 'index.html',{'movies':pages, 'populars':populars, 'filter':filter})
+    return render(request, 'index.html', {
+        'movies': pages,
+        'populars': populars,
+        'filter': query
+    })
 
 class MediaDetailView(generic.DetailView):
     model = Media
@@ -54,6 +71,7 @@ class MoviesListView(generic.ListView):
     context_object_name = 'medias'
     template_name = 'medias_list.html'
     paginate_by = 18
+    ordering = ['-lancament_date']
 
     def get_queryset(self):
         qs = Media.objects.filter(typeMedia='filme')
@@ -76,6 +94,7 @@ class SeriesListView(generic.ListView):
     context_object_name = 'medias'
     template_name = 'medias_list.html'
     paginate_by = 18
+    ordering = ['-lancament_date']
     
     def get_queryset(self):
         qs = Media.objects.filter(typeMedia='serie')
